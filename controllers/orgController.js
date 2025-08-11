210821d79b65f207d32a4bea99c8cb31a35b8050
Puneet Singh <puneet.singh@loginradius.com>
2025-08-11 14:17:50 +0530

    Invitation API

--- controllers/orgController.js
+++ controllers/orgController.js
@@ -23,59 +23,105 @@ const createUser = async (req, res) => {
 };
 
 export const createOrg = async (req, res) => {
-    try {
-      const { name } = req.body;
-  
-      if (!name) {
-        return res.status(400).json({
-          success: false,
-          message: 'name is required in request body',
-        });
-      }
+  try {
+    const { name } = req.body;
 
-  
-      // Step 1: Create Organization
-      const orgResponse = await privateClient.post(
-        `${process.env.THIRD_PARTY_API}/v2/manage/organizations`,
-        { name }, // POST body
-        {
-          params: {
-            apikey: process.env.API_KEY,
-            apisecret: process.env.API_SECRET,
-          },
-        }
-      );
-
-      const orgData = orgResponse.data;
-
-      // Step 2: Add user to organization
-      const ownershipResponse = await privateClient.put(
-        `${process.env.THIRD_PARTY_API}/v2/manage/account/${req.user.Uid}/orgcontext/${orgData.Id}/roles`,
-        { RoleIds: [CONSTANT.OWNER_ROLE_ID] },
-        {
-          params: {
-            apikey: process.env.API_KEY,
-            apisecret: process.env.API_SECRET,
-          },
-        }
-      );
-  
-      res.json({
-        success: true
-      });
-  
-    } catch (error) {
-      if (error.response) {
-        // Return exactly what the API sent
-        return res
-          .status(error.response.status || 500)
-          .json(error.response.data);
-      }
-  
-      res.status(500).json({
-        Description: 'Internal Server Error',
-        ErrorCode: null,
-        Message: error.message,
+    if (!name) {
+      return res.status(400).json({
+        success: false,
+        message: 'name is required in request body',
       });
     }
-  };
+
+
+    // Step 1: Create Organization
+    const orgResponse = await privateClient.post(
+      `${process.env.THIRD_PARTY_API}/v2/manage/organizations`,
+      { name }, // POST body
+      {
+        params: {
+          apikey: process.env.API_KEY,
+          apisecret: process.env.API_SECRET,
+        },
+      }
+    );
+
+    const orgData = orgResponse.data;
+
+    // Step 2: Add user to organization
+    const ownershipResponse = await privateClient.put(
+      `${process.env.THIRD_PARTY_API}/v2/manage/account/${req.user.Uid}/orgcontext/${orgData.Id}/roles`,
+      { RoleIds: [CONSTANT.OWNER_ROLE_ID] },
+      {
+        params: {
+          apikey: process.env.API_KEY,
+          apisecret: process.env.API_SECRET,
+        },
+      }
+    );
+
+    res.json({
+      success: true
+    });
+
+  } catch (error) {
+    if (error.response) {
+      // Return exactly what the API sent
+      return res
+        .status(error.response.status || 500)
+        .json(error.response.data);
+    }
+
+    res.status(500).json({
+      Description: 'Internal Server Error',
+      ErrorCode: null,
+      Message: error.message,
+    });
+  }
+};
+
+
+export const sendInvitation = async (req, res) => {
+  try {
+    const { roleId, email, orgId } = req.body;
+
+    let payload = {
+      "Email": email,
+      "OrgId": orgId,
+      "RoleIds": [roleId],
+      "InvitationUrl": process.env.FRONTEND_URL,
+      "InviterUid": req.user.Uid
+    }
+
+    console.log(payload)
+    // Step 1: send Inviation
+    const invitationResponse = await privateClient.post(
+      `${process.env.THIRD_PARTY_API}/v2/manage/invitations`,
+      payload,
+      {
+        params: {
+          apikey: process.env.API_KEY,
+          apisecret: process.env.API_SECRET,
+        },
+      }
+    );
+
+    res.json({
+      success: true
+    });
+
+  } catch (error) {
+    if (error.response) {
+      // Return exactly what the API sent
+      return res
+        .status(error.response.status || 500)
+        .json(error.response.data);
+    }
+
+    res.status(500).json({
+      Description: 'Internal Server Error',
+      ErrorCode: null,
+      Message: error.message,
+    });
+  }
+};
--- routes/orgRoutes.js
+++ routes/orgRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createOrg } from "../controllers/orgController.js";
+import { createOrg, sendInvitation } from "../controllers/orgController.js";
 import { authenticateUser } from '../middleware/authMiddleware.js';
 
 
 const router = express.Router();
 
 router.post("/",  authenticateUser, createOrg);
+router.post("/invite",  authenticateUser, sendInvitation);
 
 export default router;
